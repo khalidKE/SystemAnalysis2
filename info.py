32bbaadfe4c8d3699e28c78ce1286557b3756b8a
Khalid Ehab <KhalidKE>
2024-12-20 18:14:50 +0200

    Q3

---
+++ info.py
@@ -0,0 +1,12 @@
+# tuple
+academic_data = ("Khalid Ehab", "2232040", "Information System", "3", "Faculty of Computers and Artificial Intelligence")
+
+# Unpack
+name, student_id, major, level, faculty = academic_data
+
+# Print
+print(f"Name: {name}")
+print(f"ID: {student_id}")
+print(f"Major: {major}")
+print(f"Level: {level}")
+print(f"Faculty: {faculty}")
